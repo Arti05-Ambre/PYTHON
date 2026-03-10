7bebc900107b5e84e9184d7d01a2931c7390b97a
Arti05-Ambre <ambrearti62@gmail.com>
2026-03-10 23:20:34 +0530

    Create sample2.py

---
+++ sample2.py
@@ -0,0 +1,11 @@
+file=open("Sample2.txt","a")
+text=input("Enter the text to append:")
+file.write(text+"\n")
+file.close()
+print("Text Successfully Appended to the file.")
+
+
+
+output
+Enter the text to append:Batch 2026
+Text Successfully Appended to the file.
