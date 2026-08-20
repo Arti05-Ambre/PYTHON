e7e432e77874833f57e0784a7ea0751cc509893c
Arti05-Ambre <ambrearti62@gmail.com>
2026-08-20 22:26:55 +0530

    Add frequency binning to frequency.py
    Implemented equal width and equal frequency binning for age data.

---
+++ frequency.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+b) Equal Frequency Binning
+b) Equal Frequency Binning 
+df = pd.read_csv("age_data.csv")
+df = pd.read_csv("age_data.csv") 
+print("Original Data:") 
+print(df) 
+# a) Equal Width Binning 
+df["Equal_Width"] = pd.cut( 
+df["Age"], 
+bins=3, 
+labels=["Young", "Adult", "Senior"] 
+) 
+print("\nEqual Width Binning:") 
+print(df) 
+# b) Equal Frequency Binning 
+df["Equal_Frequency"] = pd.qcut( 
+df["Age"], 
+q=3, 
+labels=["Young", "Adult", "Senior"] 
+) 
+print("\nEqual Frequency  Binning:") 
+print(df)
